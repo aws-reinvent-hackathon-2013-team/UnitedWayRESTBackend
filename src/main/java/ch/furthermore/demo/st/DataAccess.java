a7404768c5a04bc44c17a1986baa20acbbe820f5
Christian Tschenett <chtz@me.com>
2013-11-12 11:49:56 -0800

    data access interface added - first version

---
+++ src/main/java/ch/furthermore/demo/st/DataAccess.java
@@ -0,0 +1,9 @@
+package ch.furthermore.demo.st;
+
+import java.util.Collection;
+
+public class DataAccess {
+	Collection<GeocodedResults> getOpportunities( float lat, float longitude, float radius ) {
+		return null; //FIXME
+	}
+}
---
+++ src/main/java/ch/furthermore/demo/st/GeocodedResults.java
@@ -0,0 +1,112 @@
+package ch.furthermore.demo.st;
+
+public class GeocodedResults {
+	private long needId;
+	private String needTitle;
+	private String description;
+	private long agencyIdBit;
+	private String agencyName;
+	private String category;
+	private String agencyAddress;
+	private String agencyCity;
+	private String agencyState;
+	private int agencyZip;
+	private String phone;
+	private String email;
+
+	public long getNeedId() {
+		return needId;
+	}
+
+	public void setNeedId(long needId) {
+		this.needId = needId;
+	}
+
+	public String getNeedTitle() {
+		return needTitle;
+	}
+
+	public void setNeedTitle(String needTitle) {
+		this.needTitle = needTitle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public long getAgencyIdBit() {
+		return agencyIdBit;
+	}
+
+	public void setAgencyIdBit(long agencyIdBit) {
+		this.agencyIdBit = agencyIdBit;
+	}
+
+	public String getAgencyName() {
+		return agencyName;
+	}
+
+	public void setAgencyName(String agencyName) {
+		this.agencyName = agencyName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getAgencyAddress() {
+		return agencyAddress;
+	}
+
+	public void setAgencyAddress(String agencyAddress) {
+		this.agencyAddress = agencyAddress;
+	}
+
+	public String getAgencyCity() {
+		return agencyCity;
+	}
+
+	public void setAgencyCity(String agencyCity) {
+		this.agencyCity = agencyCity;
+	}
+
+	public String getAgencyState() {
+		return agencyState;
+	}
+
+	public void setAgencyState(String agencyState) {
+		this.agencyState = agencyState;
+	}
+
+	public int getAgencyZip() {
+		return agencyZip;
+	}
+
+	public void setAgencyZip(int agencyZip) {
+		this.agencyZip = agencyZip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
